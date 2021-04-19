d3c581d2a35b483700e9310269c7f3b32915253e
Adrian <adrian_zinko@op.pl>
2021-04-19 13:51:11 +0200

    Update boards layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Grid } from '@material-ui/core'
+import React from "react";
+import { Grid } from "@material-ui/core";
 
-import Details from './components/Details/Details'
-import MainBoard from './components/MainBoard/MainBoard';
-import useStyles from './styles';
+import Details from "./components/Details/Details";
+import MainBoard from "./components/MainBoard/MainBoard";
+import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
 
   return (
     <>
-      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
-        <Grid item xs={12} sm={4}>
-          <Details title="Income"/>
-        </Grid>
-        <Grid item xs={12} sm={3}>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="space-around"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={12} md={6}>
           <MainBoard />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Details title="Expense"/>
+        <Grid item xs={12} sm={12} md={5}>
+          <Details title="Income" />
+          <Details title="Expense" />
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -24,7 +24,7 @@ export default makeStyles((theme) => ({
   },
   grid: {
     '& > *': {
-      margin: theme.spacing(2),
+      margin: theme.spacing(1),
     },
   },
 }));
