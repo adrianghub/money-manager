97daba39e5fdad88c140d653f37c3d4e820e6d68
Adrian <adrian_zinko@op.pl>
2021-04-19 17:34:27 +0200

    Define logic for context actions

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,7 +1,28 @@
 // Reducer => a function that takes in the old state, and an action => new state ...
 
+const transactions = [
+  { id: 1, }
+  { id: 2, },
+  { id: 3, },
+]
+
 const contextReducer = (state, action) => {
-  
+
+  let transactions;
+
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      transactions = state.filter(t => t.id !== action.payload);
+      return transactions;
+    
+    case 'ADD_TRANSACTION':
+      transactions = [action.payload, ...state]
+      return transactions;
+
+    default:
+      return state;
+  }
+
 }
 
 export default contextReducer;
