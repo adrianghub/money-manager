3361e99e8c6123b37e8194c5337ae621e0bb0a83
Adrian <adrian_zinko@op.pl>
2021-04-20 20:27:38 +0200

    Format date

--- src/components/MainBoard/Form/Form.jsx
+++ src/components/MainBoard/Form/Form.jsx
@@ -5,12 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 import useStyles from './styles';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import formatDate from '../../../utils/formatDate';
 
 const initialState = {
   amount: '',
   category: '',
   type: 'Income',
-  date: new Date(),
+  date: formatDate(new Date()),
 }
 
 const Form = () => {
@@ -57,7 +58,7 @@ const Form = () => {
         <TextField type="number" label="Amout" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })}/>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
       </Grid>
       <Button className={classes.transaction} varient="contained" color="inherit"  onClick={submitTransaction}>Submit</Button>
     </Grid>
