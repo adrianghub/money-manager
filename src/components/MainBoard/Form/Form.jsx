a1d46ca4e1eb6e7d777cc1271ccd890b3fa8b22e
Adrian <adrian_zinko@op.pl>
2021-05-09 19:25:46 +0200

    add transaction data using speechly

--- src/components/MainBoard/Form/Form.jsx
+++ src/components/MainBoard/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import {
   TextField,
   Typography,
@@ -48,6 +48,42 @@ const Form = () => {
     // Reset state to add new transaction
     setFormData(initialState);
   };
+
+  useEffect(() => {
+    if(segment) {
+      if(segment.intent.intent === 'add_expense') {
+        setFormData({ ...formData, type: 'Expense' })
+      } else if (segment.intent.intent === 'add_income') {
+        setFormData({ ...formData, type: 'Income' })
+      } else if (segment.isFinal && segment.intent.intent === "create_transaction") {
+        submitTransaction();
+      } else if (segment.isFinal && segment.intent.intent === "cancel_transaction") {
+        return setFormData(initialState);
+      }
+      
+      segment.entities.forEach((e) => {
+        const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`
+
+        switch(e.type) {
+          case 'amount':
+            setFormData({ ...formData, amount: e.value })
+            break;
+          case 'category':
+            if(incomeCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: 'Income', category })
+            } else if (expenseCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: 'Expense', category })
+            }
+            break;
+          case 'date':
+            setFormData({ ...formData, date: e.value })
+            break;
+          default:
+            break;
+        }
+      })
+    }
+  }, [segment]);
 
   const selectedCategories =
     formData.type === "Income" ? incomeCategories : expenseCategories;
